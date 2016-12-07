3a13b4827e765616bf1fbeabd1d2054a13385d97
Student <Student@PC-120>
2016-12-07 17:38:01 +0800

    Done

---
+++ Documents/NetBeansProjects/G10/src/g10/G10.java
@@ -0,0 +1,18 @@
+
+package g10;
+
+
+public class G10 {
+
+    
+    public static void main(String[] args) {
+        int a=3, b=2, c=1;
+            if (a>c){
+                a = b = c = 1;
+
+                System.out.println(b);
+            }
+
+    }
+    
+}
